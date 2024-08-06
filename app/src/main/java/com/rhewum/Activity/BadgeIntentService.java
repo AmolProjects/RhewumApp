b7f0384a840c662449c6404477937bf3a2aa957d
Amol <junghareamol11@gmail.com>
2024-08-06 11:24:08 +0530

    Badge Notification

---
+++ app/src/main/java/com/rhewum/Activity/BadgeIntentService.java
@@ -0,0 +1,80 @@
+package com.rhewum.Activity;
+
+import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.Nullable;
+
+import com.rhewum.R;
+
+import me.leolin.shortcutbadger.ShortcutBadger;
+
+public class BadgeIntentService extends IntentService {
+
+    private static final String NOTIFICATION_CHANNEL = "me.leolin.shortcutbadger.example";
+
+    private int notificationId = 0;
+
+    public BadgeIntentService() {
+        super("BadgeIntentService");
+    }
+
+    private NotificationManager mNotificationManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        super.onStart(intent, startId);
+    }
+
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            int badgeCount = intent.getIntExtra("badgeCount", 0);
+
+            mNotificationManager.cancel(notificationId);
+            notificationId++;
+
+            Notification.Builder builder = new Notification.Builder(getApplicationContext())
+                    .setContentTitle("")
+                    .setContentText("")
+                    .setSmallIcon(R.drawable.logo);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                setupNotificationChannel();
+
+                builder.setChannelId(NOTIFICATION_CHANNEL);
+            }
+
+            Notification notification = builder.build();
+            ShortcutBadger.applyNotification(getApplicationContext(), notification, badgeCount);
+            mNotificationManager.notify(notificationId, notification);
+        }
+    }
+
+    private void setupNotificationChannel() {
+
+
+        NotificationChannel channel = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel(NOTIFICATION_CHANNEL, "ShortcutBadger Sample",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            mNotificationManager.createNotificationChannel(channel);
+        }
+
+    }
+}
